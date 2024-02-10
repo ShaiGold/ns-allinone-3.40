a759ee4195d285e78e89d747cb016c558f199220
ShaiGold <157375899+ShaiGold@users.noreply.github.com>
2024-02-10 16:58:19 +0200

    Update tcp-bbr-example.cc

--- ns-3.40/scratch/tcp-bbr-example.cc
+++ ns-3.40/scratch/tcp-bbr-example.cc
@@ -83,6 +83,22 @@ TraceThroughput (Ptr<FlowMonitor> monitor)
   prev = itr->second.txBytes;
 
   Simulator::Schedule(Seconds(0.2), &TraceThroughput, monitor);}
+// Calculate throughput
+static void
+TraceThroughput2 (Ptr<FlowMonitor> monitor)
+{
+  FlowMonitor::FlowStatsContainer stats = monitor->GetFlowStats();
+  auto itr = stats.begin();
+  Time curTime = Now();
+  std::ofstream thr(dir + "/throughput2.dat", std::ios::out | std::ios::app);
+
+  // Convert time to seconds and use GetSeconds()
+  thr << curTime.GetSeconds() << " " << 8 * (itr->second.txBytes - prev) / (1000 * 1000 * (curTime.GetSeconds() - prevTime.GetSeconds())) << std::endl;
+
+  prevTime = curTime;
+  prev = itr->second.txBytes;
+
+  Simulator::Schedule(Seconds(0.2), &TraceThroughput2, monitor);}
 
 // Check the queue size
 void CheckQueueSize (Ptr<QueueDisc> qd)
@@ -105,6 +121,19 @@ void TraceCwnd (uint32_t nodeId, uint32_t socketId)
   AsciiTraceHelper ascii;
   Ptr<OutputStreamWrapper> stream = ascii.CreateFileStream (dir + "/cwnd.dat");
   Config::ConnectWithoutContext ("/NodeList/" + std::to_string (nodeId) + "/$ns3::TcpL4Protocol/SocketList/" + std::to_string (socketId) + "/CongestionWindow", MakeBoundCallback (&CwndTracer, stream));
+}
+
+// Trace congestion window2
+static void CwndTracer2 (Ptr<OutputStreamWrapper> stream, uint32_t oldval, uint32_t newval)
+{
+  *stream->GetStream () << Simulator::Now ().GetSeconds () << " " << newval / 1448.0 << std::endl;
+}
+
+void TraceCwnd2 (uint32_t nodeId, uint32_t socketId)
+{
+  AsciiTraceHelper ascii;
+  Ptr<OutputStreamWrapper> stream = ascii.CreateFileStream (dir + "/cwnd2.dat");
+  Config::ConnectWithoutContext ("/NodeList/" + std::to_string (nodeId) + "/$ns3::TcpL4Protocol/SocketList/" + std::to_string (socketId) + "/CongestionWindow", MakeBoundCallback (&CwndTracer2, stream));
 }
 
 int main (int argc, char *argv [])
@@ -148,7 +177,10 @@ int main (int argc, char *argv [])
   sender.Create (1);
   receiver.Create (1);
   routers.Create (2);
-
+  NodeContainer sender2, receiver2;
+  sender2.Create (1);
+  receiver2.Create (1);
+  
   // Create the point-to-point link helpers
   PointToPointHelper bottleneckLink;
   bottleneckLink.SetDeviceAttribute  ("DataRate", StringValue ("10Mbps"));
@@ -157,29 +189,52 @@ int main (int argc, char *argv [])
   PointToPointHelper edgeLink;
   edgeLink.SetDeviceAttribute  ("DataRate", StringValue ("1000Mbps"));
   edgeLink.SetChannelAttribute ("Delay", StringValue ("5ms"));
+  
+  PointToPointHelper edgeLink25ms;
+  edgeLink25ms.SetDeviceAttribute  ("DataRate", StringValue ("1000Mbps"));
+  edgeLink25ms.SetChannelAttribute ("Delay", StringValue ("25ms"));
+  
+  PointToPointHelper bottleneckLink2;
+  bottleneckLink.SetDeviceAttribute  ("DataRate", StringValue ("10Mbps"));
+  bottleneckLink.SetChannelAttribute ("Delay", StringValue ("10ms"));
 
+  PointToPointHelper edgeLink2;
+  edgeLink.SetDeviceAttribute  ("DataRate", StringValue ("1000Mbps"));
+  edgeLink.SetChannelAttribute ("Delay", StringValue ("5ms"));
+  
   // Create NetDevice containers
   NetDeviceContainer senderEdge = edgeLink.Install (sender.Get (0), routers.Get (0));
   NetDeviceContainer r1r2 = bottleneckLink.Install (routers.Get (0), routers.Get (1));
   NetDeviceContainer receiverEdge = edgeLink.Install (routers.Get (1), receiver.Get (0));
+  
 
+  NetDeviceContainer senderEdge2 = edgeLink25ms.Install (sender2.Get (0), routers.Get (0));
+  NetDeviceContainer r1r2_2 = bottleneckLink2.Install (routers.Get (0), routers.Get (1));
+  NetDeviceContainer receiverEdge2 = edgeLink2.Install (routers.Get (1), receiver2.Get (0));
   // Install Stack
   InternetStackHelper internet;
   internet.Install (sender);
   internet.Install (receiver);
   internet.Install (routers);
 
+  InternetStackHelper internet2;
+  internet2.Install (sender2);
+  internet2.Install (receiver2);
+  internet.Install (routers);
   // Configure the root queue discipline
-  TrafficControlHelper tch;
+  TrafficControlHelper tch,tch2;
   tch.SetRootQueueDisc (queueDisc);
-
+  tch2.SetRootQueueDisc (queueDisc);
   if (bql)
     {
       tch.SetQueueLimits ("ns3::DynamicQueueLimits", "HoldTime", StringValue ("1000ms"));
+      tch2.SetQueueLimits ("ns3::DynamicQueueLimits", "HoldTime", StringValue ("1000ms"));
     }
 
   tch.Install (senderEdge);
+  tch2.Install (senderEdge2);
   tch.Install (receiverEdge);
+  tch2.Install (receiverEdge2);
 
   // Assign IP addresses
   Ipv4AddressHelper ipv4;
@@ -192,6 +247,18 @@ int main (int argc, char *argv [])
 
   ipv4.NewNetwork ();
   Ipv4InterfaceContainer ir1 = ipv4.Assign (receiverEdge);
+  
+
+  Ipv4AddressHelper ipv4_2;
+  ipv4_2.SetBase ("10.1.0.0", "255.255.255.0");
+
+  Ipv4InterfaceContainer i1i2_2 = ipv4_2.Assign (r1r2_2);
+
+  ipv4_2.NewNetwork ();
+  Ipv4InterfaceContainer is1_2 = ipv4_2.Assign (senderEdge2);
+
+  ipv4_2.NewNetwork ();
+  Ipv4InterfaceContainer ir1_2 = ipv4_2.Assign (receiverEdge2);
 
   // Populate routing tables
   Ipv4GlobalRoutingHelper::PopulateRoutingTables ();
@@ -201,17 +268,27 @@ int main (int argc, char *argv [])
 
   // Install application on the sender
   BulkSendHelper source ("ns3::TcpSocketFactory", InetSocketAddress (ir1.GetAddress (1), port));
+  BulkSendHelper source2 ("ns3::TcpSocketFactory", InetSocketAddress (ir1_2.GetAddress (1), port));
   source.SetAttribute ("MaxBytes", UintegerValue (0));
+  source2.SetAttribute ("MaxBytes", UintegerValue (0));
   ApplicationContainer sourceApps = source.Install (sender.Get (0));
+  ApplicationContainer sourceApps2 = source2.Install (sender2.Get (0));
   sourceApps.Start (Seconds (0.0));
+  sourceApps2.Start (Seconds (0.0));
   Simulator::Schedule (Seconds (0.2), &TraceCwnd, 0, 0);
+  Simulator::Schedule (Seconds (0.2), &TraceCwnd2, 0, 0);
   sourceApps.Stop (stopTime);
+  sourceApps2.Stop (stopTime);
 
   // Install application on the receiver
   PacketSinkHelper sink ("ns3::TcpSocketFactory", InetSocketAddress (Ipv4Address::GetAny (), port));
+  PacketSinkHelper sink2 ("ns3::TcpSocketFactory", InetSocketAddress (Ipv4Address::GetAny (), port));
   ApplicationContainer sinkApps = sink.Install (receiver.Get (0));
+  ApplicationContainer sinkApps2 = sink2.Install (receiver2.Get (0));
   sinkApps.Start (Seconds (0.0));
+  sinkApps2.Start (Seconds (0.0));
   sinkApps.Stop (stopTime);
+  sinkApps2.Stop (stopTime);
 
   // Create a new directory to store the output of the program
   dir = "bbr-results/" + currentTime + "/";
@@ -247,6 +324,10 @@ int main (int argc, char *argv [])
   FlowMonitorHelper flowmon;
   Ptr<FlowMonitor> monitor = flowmon.InstallAll ();
   Simulator::Schedule (Seconds (0 + 0.000001), &TraceThroughput, monitor);
+
+  FlowMonitorHelper flowmon2;
+  Ptr<FlowMonitor> monitor2 = flowmon2.InstallAll ();
+  Simulator::Schedule (Seconds (0 + 0.000001), &TraceThroughput2, monitor2);
 
   Simulator::Stop (stopTime + TimeStep (1));
   Simulator::Run ();
