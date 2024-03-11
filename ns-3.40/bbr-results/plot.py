5ef8c63fcf4badc5400f8e9162b82e158be5f3e8
ShaiGold <gh@campus.technion.ac.il>
2024-03-11 21:41:34 +0200

    better plot

--- ns-3.40/bbr-results/plot.py
+++ ns-3.40/bbr-results/plot.py
@@ -29,28 +29,27 @@ filtered_data = [(src, time, throughput) for time, src, dst, throughput in data 
 source_1_data = [(time, throughput) for src, time, throughput in filtered_data if '10.2.1.1' in src]
 source_2_data = [(time, throughput) for src, time, throughput in filtered_data if '10.2.2.1' in src]
 
-# Plotting
-plt.subplot(2, 1, 1)  # 2 rows, 1 column, first subplot
-plt.plot(*zip(*source_1_data), label='10.2.1.1')
-plt.plot(*zip(*source_2_data), label='10.2.2.1')
+# Plotting# Create two subplots with shared x-axis
+fig, (ax1, ax2) = plt.subplots(2, 1, sharex=True)
 
-# Adding labels and title
-plt.xlabel('Time (s)')
-plt.ylabel('Throughput')
-plt.title('Throughput over Time for RTT X<ratio>')
-plt.legend()
+# Plotting throughput data on the first subplot
+ax1.plot(*zip(*source_1_data), label='10.2.1.1')
+ax1.plot(*zip(*source_2_data), label='10.2.2.1')
+ax1.set_ylabel('Throughput')
+ax1.set_title('Throughput over Time for RTT X<ratio>')
+ax1.legend()
 
-#plotting the throughput ratio graph
-plt.subplot(2, 1, 2)  # 2 rows, 1 column, second subplot
-throughput1 = np.array([throughput for time,throughput in source_1_data])
-throughput2 = np.array([throughput for time,throughput in source_2_data])
-time_arr = [time for time,throughput in source_1_data]
+# Plotting the throughput ratio graph on the second subplot
+throughput1 = np.array([throughput for time, throughput in source_1_data])
+throughput2 = np.array([throughput for time, throughput in source_2_data])
+time_arr = [time for time, throughput in source_1_data]
 
-plt.plot(time_arr, throughput1/throughput2)
-plt.xlabel('Time (s)')
-plt.ylabel('Throughput1/Throughput2')
+ax2.plot(time_arr, throughput1 / throughput2)
+ax2.set_xlabel('Time (s)')
+ax2.set_ylabel('Throughput1/Throughput2')
 
+# Adjust layout to prevent overlapping
 plt.tight_layout()
+
 # Display the plot
 plt.show()
-
