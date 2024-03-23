8efa2009724b5d6bd26434a1f3e8715ce3cda419
ShaiGold <gh@campus.technion.ac.il>
2024-03-23 19:14:34 +0200

    updated plot for only one sender and receiver

--- ns-3.40/bbr-results/plot.py
+++ ns-3.40/bbr-results/plot.py
@@ -30,26 +30,34 @@ source_1_data = [(time, throughput) for src, time, throughput in filtered_data i
 source_2_data = [(time, throughput) for src, time, throughput in filtered_data if '10.2.2.1' in src]
 
 # Plotting# Create two subplots with shared x-axis
-fig, (ax1, ax2) = plt.subplots(2, 1, sharex=True)
-
+if len(source_2_data):
+    fig, (ax1, ax2) = plt.subplots(2, 1, sharex=True)
+else:
+    fig , ax1 = plt.subplots(1,1)
 # Plotting throughput data on the first subplot
 ax1.plot(*zip(*source_1_data), label='10.2.1.1')
 ax1.plot(*zip(*source_2_data), label='10.2.2.1')
 ax1.set_ylabel('Throughput')
-ax1.set_title('Throughput over Time for RTT X<ratio>')
+ax1.set_title('Throughput over Time')
 ax1.legend()
 
 # Plotting the throughput ratio graph on the second subplot
 throughput1 = np.array([throughput for time, throughput in source_1_data])
 throughput2 = np.array([throughput for time, throughput in source_2_data])
 time_arr = [time for time, throughput in source_1_data]
+if len(source_2_data):
+    ax2.plot(time_arr, throughput1 / throughput2, label='Throughput Ratio')
+    ax2.set_ylim(0, 1)
+    ax2.axhline(y=np.median(throughput1)/np.median(throughput2), color='red', linestyle='--', label='Median(throughput1) / Median(throughput2)')
+    ax2.set_xlabel('Time (s)')
+    ax2.set_ylabel('Throughput1/Throughput2')
+    ax2.legend()
+    print('Median(throughput1) / Median(throughput2):',
+          np.median(throughput1)/np.median(throughput2))
+else:
+    throughput2 = np.zeros_like(throughput1)
+    ax1.set_xlabel('Time (s)')
 
-ax2.plot(time_arr, throughput1 / throughput2, label='Throughput Ratio')
-ax2.axhline(y=np.median(throughput1)/np.median(throughput2), color='red', linestyle='--', label='Median(throughput1) / Median(throughput2)')
-ax2.set_xlabel('Time (s)')
-ax2.set_ylabel('Throughput1/Throughput2')
-ax2.legend()
-print('Median(throughput1) / Median(throughput2):',np.median(throughput1)/np.median(throughput2)) #0.19203690216071864
 print('utilization', (np.average(throughput1)+np.average(throughput2))/10)
 # Adjust layout to prevent overlapping
 plt.tight_layout()
